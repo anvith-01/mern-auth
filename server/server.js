3148dd92b08246271eb1a4613221984555265c55
Anvith Reddy <anvithrdy2003@gmail.com>
2025-02-18 13:27:45 +0530

    Project Completed

---
+++ server/server.js
@@ -0,0 +1,36 @@
+
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from './routes/authRouters.js'
+import userRouter from "./routes/userRoutes.js";
+
+
+const app = express();
+const port = process.env.PORT || 4000
+
+connectDB();
+
+
+const allowedOrigins = ['http://localhost:5173']
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin:allowedOrigins, credentials:true}));
+
+
+
+//API end points
+app.get('/',(req,res)=>{
+    res.send("hello world");
+});
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
+
+
+app.listen(port,()=>{
+    console.log(`server started on PORT ${port}`);
+});
